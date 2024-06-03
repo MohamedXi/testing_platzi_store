039e93623d29145cb23249ed0e03d0a9657e28bb
Ismael MOHAMED <i.mohamed@groupe-creative.fr>
2024-06-03 16:46:06 +0200

    chore: adding the describe into test

--- src/components/ProductList.test.tsx
+++ src/components/ProductList.test.tsx
@@ -1,5 +1,52 @@
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
+import { ProductState } from '../store/product/productTypes';
+import ProductList from './ProductList';
 
 const mockStore = configureStore([]);
 
-describe('ProductList', () => {});
+describe('ProductList', () => {
+  test('renders loading state', () => {
+    const initialState: ProductState = { loading: true, products: [], error: null };
+    const store = mockStore({ products: initialState });
+
+    render(
+      <Provider store={store}>
+        <ProductList />
+      </Provider>
+    );
+
+    expect(screen.getByText(/loading/i)).toBeInTheDocument();
+  });
+
+  test('renders products', () => {
+    const initialState: ProductState = {
+      loading: false,
+      products: [{ id: 1, title: 'Product 1', price: 100, description: '', category: '', image: '' }],
+      error: null,
+    };
+    const store = mockStore({ products: initialState });
+
+    render(
+      <Provider store={store}>
+        <ProductList />
+      </Provider>
+    );
+
+    expect(screen.getByText('Product 1')).toBeInTheDocument();
+  });
+
+  test('renders error state', () => {
+    const initialState: ProductState = { loading: false, products: [], error: 'Error fetching products' };
+    const store = mockStore({ products: initialState });
+
+    render(
+      <Provider store={store}>
+        <ProductList />
+      </Provider>
+    );
+
+    expect(screen.getByText(/error fetching products/i)).toBeInTheDocument();
+  });
+});
